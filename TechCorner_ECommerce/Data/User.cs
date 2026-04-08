c6ef6789ffb2992b1099839cf8b498a000c71489
Thinh Le <lht1823k@gmail.com>
2026-04-08 12:02:44 +0700

    Init  project

---
+++ TechCorner_ECommerce/Data/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TechCorner_ECommerce.Data {
+    public class User {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
